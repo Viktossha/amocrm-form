3639ce06fb17d5bebb74e2067b7ecbe509ecdaa7
Vika <vikapetrova8@gmail.com>
2025-07-09 23:51:03 +0300

    setup redux store and add Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {createRoot} from 'react-dom/client'
 import {App} from './App.tsx'
+import {store} from "./store.ts";
+import {Provider} from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
 )
---
+++ frontend/src/store.ts
@@ -0,0 +1,15 @@
+import {configureStore, type ThunkDispatch, type UnknownAction} from "@reduxjs/toolkit";
+import {leadReducer, leadSlice} from "./model/leadSlice.ts";
+
+export const store = configureStore({
+    reducer: {
+        [leadSlice.name]: leadReducer
+    },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+
+// @ts-ignore
+window.store = store
