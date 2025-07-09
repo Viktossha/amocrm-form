08243ad305c8a52136f14f02926fadbaa9ebb873
Vika <vikapetrova8@gmail.com>
2025-07-09 21:29:26 +0300

    add contact and lead creation logic for AmoCRM

---
+++ backend/lead.php
@@ -0,0 +1,91 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$domain = $_ENV['AMOCRM_DOMAIN'];
+$accessToken = $_ENV['AMOCRM_TOKEN'];
+$headers = [
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $accessToken,
+];
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$contactData = [
+    [
+        "name" => $data['name'],
+        "custom_fields_values" =>[
+            [
+                "field_id" => 711283,
+                "values" => [
+                    [
+                        "value" => $data['phone']
+                    ]
+                ]
+            ],
+            [
+                "field_id" => 711285,
+                "values" => [
+                    [
+                        "value" => $data['email']
+                    ]
+                ]
+            ]
+        ]
+    ],
+];
+
+$ch = curl_init("https://$domain/api/v4/contacts");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($contactData));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+curl_close($ch);
+$result = json_decode($response, true);
+$contactId = $result['_embedded']['contacts'][0]['id'];
+
+if ($contactId) {
+
+    $leadData = [
+        [
+            "name" => 'Заявка с формы',
+            "price" => (int) $data['price'],
+            "_embedded" => [
+                "contacts" => [
+                    ["id" => $contactId]
+                ]
+            ],
+            "custom_fields_values" => [
+                [
+                    "field_id" => 715033,
+                    "values" => [
+                        [
+                            "value" => true
+                        ]
+                    ]
+                ],
+            ]
+        ],
+    ];
+
+    $ch = curl_init("https://$domain/api/v4/leads");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($leadData));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $response = curl_exec($ch);
+    curl_close($ch);
+}
